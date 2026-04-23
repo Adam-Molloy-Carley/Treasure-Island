e5b218efc0a2708db71175302afafd267ba12bd0
Adam Molloy <g00485150@atu.ie>
2026-04-23 23:46:19 +0100

    Added X to mark visited tiles, Player Movement system, added framework in preperation of player score tracking and treasure value assignment.

--- Treasure Island/Treasure Island/TreasureMap.cpp
+++ Treasure Island/Treasure Island/TreasureMap.cpp
@@ -3,18 +3,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>   
+#include <time.h>  
 
 void setPlayerNames(char player1[20], char player2[20]);
+void gameStart(char player1[], char player2[], char TreasureMap[6][6],
+    int *p1Score, int *p2Score, int *p1Row, int *p1Col, int *p2Row, 
+    int *p2Col);
 
 int main() {
     char player1[20] = "Player1";
     char player2[20] = "Player2";
     int menuOption;
-	
-    srand(time(NULL)); 
+    int menuSet = 1;
+	int Player1Wins = 0;
+	int Player2Wins = 0;
+	int Player1Treasure = 0;
+	int Player2Treasure = 0;
+	int Player1Row, Player1Col;
+	int Player2Row, Player2Col;
 
-    int TreasureMap[6][6] = {
+    srand(time(NULL));
+
+    char TreasureMap[6][6] = {
         {'.','.','.','.','.','.'},
         {'.','.','.','.','.','.'},
         {'.','.','.','.','.','.'},
@@ -23,7 +33,7 @@ int main() {
         {'.','.','.','.','.','.'}
     };
 
-    while (1) {
+    while (menuSet) {
         printf("\n=== Treasure Island ===\n");
 		printf("Welcome to Treasure Island, %s and %s!\n", player1, player2);
         printf("1. Start the game!\n");
@@ -36,7 +46,9 @@ int main() {
         scanf("%d", &menuOption);
 
         if (menuOption == 1) {
-            printf("Starting the game...\n");
+            printf("Starting the game...\n\n");
+			gameStart(player1, player2, TreasureMap, &Player1Treasure, &Player2Treasure, &Player1Row, &Player1Col, &Player2Row, &Player2Col);
+            menuSet = 0;
         }
         else if (menuOption == 2) {
             printf("Setting player names...\n");
@@ -76,4 +88,53 @@ void setPlayerNames(char player1[20], char player2[20])
 
     printf("Player 1: %s\n", player1);
     printf("Player 2: %s\n", player2);
+}
+
+void gameStart(char player1[], char player2[], char TreasureMap[6][6], int *p1Score, int *p2Score, int *p1Row, int *p1Col, int *p2Row, int *p2Col)
+{
+    int targetRow = 0;
+    int targetCol = 0;
+
+        printf("==============================================\n");
+        printf("=   This Island is Brimming with Treasure!   =\n");
+        printf("=   See Who can dig up the most treasure!    =\n");
+        printf("==============================================\n");
+    while (*p1Score < 50 && *p2Score < 50) {
+        printf("==============================================\n");
+        printf("= -%s's treasure-", player1);
+        printf("= -%s's treasure- =\n", player2);
+        printf("==============================================\n\n");
+		printf("%s, enter the row and column to dig (0-5): ", player1);
+		scanf("%d %d", &targetRow, &targetCol);
+		
+          if (targetRow >= 0 && targetCol < 6 && targetCol >= 0 && targetRow < 6) {
+            *p1Row = targetRow;
+            *p1Col = targetCol;
+        }
+        else {
+            printf("Invalid move!\n");
+        }
+        
+        printf("%s, enter the row and column to dig (0-5): ", player2);
+		scanf("%d %d", targetRow, targetCol);
+
+        if (targetRow >= 0 && targetCol < 6 && targetCol >= 0 && targetRow < 6) {
+            *p2Row = targetRow;
+            *p2Col = targetCol;
+        }
+        else {
+            printf("Invalid move!\n");
+        }
+           
+        TreasureMap[*p1Row][*p1Col] = 'X';
+        TreasureMap[*p2Row][*p2Col] = 'X';
+        
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                printf(" %c ", TreasureMap[i][j]);
+            }
+            printf("\n");
+        }
+            
+    }
 }
